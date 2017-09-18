741603ef89e1a3880b017535d81155c7ffeccb8d
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-09-18 22:24:15 +0200

    Bypassing 405 status put by swagger when no request handler is defined

--- index.js
+++ index.js
@@ -3,29 +3,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
-var https = require('https');
+// var https = require('https');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
+// var cors = require('cors');
 
 var port = (process.env.PORT || 10081);
 var securePort = (process.env.SECURE_PORT || 10044);
 var app = express();
 
-app.enable('trust proxy');
+// Bypassing 405 status put by swagger when no request handler is defined
+app.options("/*", (req, res, next) => {
+  return res.sendStatus(200);
+});
+
+// app.enable('trust proxy');
 app.use(bodyParser.json({
   limit: '50mb'
 }));
+// app.use(cors());
 
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host.split(':')[0] + ':' + securePort + req.url);
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.secure) {
+//     // request was via https, so do no special handling
+//     next();
+//   } else {
+//     // request was via http, so redirect to https
+//     res.redirect('https://' + req.headers.host.split(':')[0] + ':' + securePort + req.url);
+//   }
+// });
 
 // swaggerRouter configuration
 var options = {
@@ -53,15 +60,16 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   app.use(middleware.swaggerUi());
 
   // Start the server
-  https.createServer({
-    key: fs.readFileSync('certs/privkey.pem'),
-    cert: fs.readFileSync('certs/cert.pem')
-  }, app).listen(securePort, function () {
-    console.log('Your module is listening on port %d (https://localhost:%d)', securePort, securePort);
-    console.log('Swagger-ui is available on https://localhost:%d/docs', securePort);
-  });
+  // https.createServer({
+  //   key: fs.readFileSync('certs/privkey.pem'),
+  //   cert: fs.readFileSync('certs/cert.pem')
+  // }, app).listen(securePort, function () {
+  //   console.log('Your module is listening on port %d (https://localhost:%d)', securePort, securePort);
+  //   console.log('Swagger-ui is available on https://localhost:%d/docs', securePort);
+  // });
 
   http.createServer(app).listen(port, function () {
-    console.log('Redirect port from %d to %d (http://localhost:%d)', port, securePort, port);
+    // console.log('Redirect port from %d to %d (http://localhost:%d)', port, securePort, port);
+    console.log('Your module is listening on port %d (https://localhost:%d)', port, port);
   });
 });
